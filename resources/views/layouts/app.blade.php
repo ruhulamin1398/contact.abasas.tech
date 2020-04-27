c5f8a805b97f8a4a887faa8361a328204612700d
ruhulaminrul <ruhul.ok@gmail.com>
2020-04-27 23:06:32 +0600

    add auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('contacts','ContactController');
+Route::resource('contacts','ContactController')->middleware('auth');
